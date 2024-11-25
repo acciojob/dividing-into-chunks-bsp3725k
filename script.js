214128ebb25c67c9fe9213a281b09bf39bb77677
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-25 16:09:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let result = []; // To store the resulting subarrays
+  let currentChunk = []; // Current subarray being formed
+  let currentSum = 0; // Current sum of elements in the chunk
+
+  for (let num of arr) {
+    // If adding the current number exceeds n
+    if (currentSum + num > n) {
+      // Push the current chunk to the result and reset
+      result.push(currentChunk);
+      currentChunk = [];
+      currentSum = 0;
+    }
+
+    // Add the current number to the chunk and update the sum
+    currentChunk.push(num);
+    currentSum += num;
+  }
+
+  // If there are remaining elements in the last chunk
+  if (currentChunk.length > 0) {
+    result.push(currentChunk);
+  }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
